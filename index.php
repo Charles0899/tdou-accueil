61545194130033cc812dff031e35b019b5cf593c
Charles0899 <charles0899@live.fr>
2020-06-25 11:48:13 +0200

    modifications

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;400;500;900&display=swap" rel="stylesheet">
- <link rel="stylesheet" href="main.css">
+ <link rel="stylesheet" href="main-charles.css">
  <link rel="icon" type="image/png" href="favicon">
  <link rel="shortcut icon" type="image/x-icon" href="favicon.png">
 </head>
